ea6d2327bce055f7da66d8e3b09fad9845fd6681
Itzchandan7899 <chandan06.bs7899@gmail.com>
2024-11-26 01:09:58 -0800

    Create for loop.c

---
+++ for loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Basic for loop that prints numbers 1 through 5
+    for (int i = 1; i <= 5; i++) {
+        printf("Iteration %d\n", i);
+    }
+    
+    return 0;
+}
