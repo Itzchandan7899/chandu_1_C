a9c79e79ced32efb14c3797b62478f130d7270a0
Itzchandan7899 <chandan06.bs7899@gmail.com>
2024-11-26 01:32:46 -0800

    Update age.c

--- age.c
+++ age.c
@@ -2,12 +2,8 @@
 
 int main() {
     int age;
-
-    // Asking the user to input their age
     printf("Enter your age: ");
     scanf("%d", &age);
-
-    // Checking if the age falls between 18 and 25 (inclusive)
     if (age >= 18 && age <= 25) {
         printf("You are a younger adult.\n");
     } else {
