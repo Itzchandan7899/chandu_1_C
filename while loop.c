943b6248a4c464251c9ac39929b9199009b4fdcb
Itzchandan7899 <chandan06.bs7899@gmail.com>
2024-11-26 01:15:11 -0800

    Create while loop.c

---
+++ while loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    // Basic while loop that prints numbers 1 through 5
+    while (i <= 5) {
+        printf("Iteration %d\n", i);
+        i++;  // Increment i after each iteration
+    }
+
+    return 0;
+}
