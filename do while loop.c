21e58dbbe2f4e61dda76c2c7c97cb88f72e6b4e5
Itzchandan7899 <chandan06.bs7899@gmail.com>
2024-11-26 01:43:21 -0800

    Update do while loop.c

--- do while loop.c
+++ do while loop.c
@@ -2,12 +2,9 @@
 
 int main() {
     int i = 1;
-
-    // do-while loop that prints numbers 1 through 5
     do {
         printf("Iteration %d\n", i);
-        i++;  // Increment i after each iteration
-    } while (i <= 5);  // Loop continues as long as i is less than or equal to 5
+    } while (i <= 5); 
 
     return 0;
 }
