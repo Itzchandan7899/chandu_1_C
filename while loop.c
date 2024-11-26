27740b09fcea5ee69c14e78ecec994a9ad7c1197
Itzchandan7899 <chandan06.bs7899@gmail.com>
2024-11-26 01:30:14 -0800

    Update while loop.c

--- while loop.c
+++ while loop.c
@@ -3,10 +3,8 @@
 int main() {
     int i = 1;
 
-    // Basic while loop that prints numbers 1 through 5
     while (i <= 5) {
         printf("Iteration %d\n", i);
-        i++;  // Increment i after each iteration
     }
 
     return 0;
